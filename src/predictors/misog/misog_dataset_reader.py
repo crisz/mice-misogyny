fa23c1302edf7361ba7313ff3de5256f7a9be2f2
crisz <crissstian96@gmail.com>
2021-11-25 17:55:13 +0100

    misog processor

--- src/predictors/misog/misog_dataset_reader.py
+++ src/predictors/misog/misog_dataset_reader.py
@@ -22,16 +22,22 @@ import tarfile
 from tqdm import tqdm as tqdm
 import numpy as np
 import math
-import pandas as pd
+from sklearn.datasets import fetch_20newsgroups
 
-from src.predictors.predictor_utils import clean_text 
+from src.predictors.predictor_utils import clean_text
 
 logger = logging.getLogger(__name__)
 
 TRAIN_VAL_SPLIT_RATIO = 0.9
 
+DATASET_PATH_BASE = Path('..') / 'data'
+DEV_PATH = DATASET_PATH_BASE / 'misogyny_EN' / 'miso_dev.tsv'
+TRAIN_PATH = DATASET_PATH_BASE / 'misogyny_EN' / 'miso_train.tsv'
+TEST_PATH = DATASET_PATH_BASE / 'misogyny_EN' / 'miso_test.tsv'
+
+
 @DatasetReader.register("newsgroups")
-class NewsgroupsDatasetReader(DatasetReader):
+class MisogDatasetReader(DatasetReader):
     def __init__(self,
                  token_indexers: Dict[str, TokenIndexer] = None,
                  tokenizer: Optional[Tokenizer] = None,
@@ -45,16 +51,6 @@ class NewsgroupsDatasetReader(DatasetReader):
         self.random_seed = 0
         np.random.seed(self.random_seed)
 
-    @staticmethod
-    def load_misogyny_dataset(path):
-        df = pd.read_csv(path, sep='\t')[['text', 'misogynous']]
-        npy = df.values
-        dataset = np.swapaxes(npy, 0, 1)
-        data, labels = dataset
-        labels = labels.astype(np.int)
-        labels = np.array(labels).reshape(-1, 1)
-        return labels, data
-
     def load_misogyny_train_dataset(self):
         return self.load_misogyny_dataset(TRAIN_PATH)
 
@@ -63,39 +59,40 @@ class NewsgroupsDatasetReader(DatasetReader):
 
     def get_data_indices(self, subset):
         np.random.seed(self.random_seed)
-        if subset in ['train', 'test']:
-            newsgroups_data = fetch_20newsgroups(
-                    subset=subset, remove=("headers", "footers", "quotes"))
-            data_indices = np.array(range(len(newsgroups_data.data))) 
-        elif subset in ['train_split', 'dev_split']:
-            newsgroups_data = fetch_20newsgroups(
-                    subset='train', remove=("headers", "footers", "quotes"))
-            data_indices = np.array(range(len(newsgroups_data.data))) 
-            np.random.shuffle(data_indices)
-            num_train = math.ceil(TRAIN_VAL_SPLIT_RATIO * len(data_indices))
-            train_indices = data_indices[:num_train]
-            val_indices = data_indices[num_train:]
-            data_indices = train_indices if subset == 'train' else val_indices 
-        else:
-            raise ValueError("Invalid value for subset")
-        return data_indices, newsgroups_data
+
+        only_train_indices = False
+        only_test_indices = False
+        if subset == 'train_split':
+            subset = 'train'
+            only_train_indices = True
+        if subset == 'dev_split':
+            subset = 'train'
+            only_test_indices = True
+
+        data = None
+        if subset == 'train':
+            labels, data = self.load_misogyny_train_dataset()
+        elif subset == 'test':
+            labels, data = self.load_misogyny_val_dataset()
+
+        data_indices = np.array(range(len(data)))
+        num_train = math.ceil(TRAIN_VAL_SPLIT_RATIO * len(data_indices))
+        if only_train_indices:
+            data_indices = data_indices[:num_train]
+        if only_test_indices:
+            data_indices = data_indices[num_train:]
+
+        return data_indices, data, labels
 
     def get_inputs(self, subset, return_labels = False):
-        np.random.seed(self.random_seed)
-        data_indices, newsgroups_data = self.get_data_indices(subset)
+        data_indices, data, labels = self.get_data_indices(subset)
         strings = [None] * len(data_indices)
         labels = [None] * len(data_indices)
         for i, idx in enumerate(data_indices):
-            txt = newsgroups_data.data[idx]
-            topic = newsgroups_data.target[idx]
-            label = newsgroups_data['target_names'][topic].split(".")[0]
-            txt = clean_text(txt, special_chars=["\n", "\t"])
-            if len(txt) == 0 or len(label) == 0:
-                strings[i] = None
-                labels[i] = None
-            else:
-                strings[i] = txt
-                labels[i] = label
+            sentence = data[idx]
+            label = labels[idx]
+            strings[i] = sentence
+            labels[i] = label
 
         strings = [x for x in strings if x is not None]
         labels = [x for x in labels if x is not None]
@@ -108,15 +105,13 @@ class NewsgroupsDatasetReader(DatasetReader):
     @overrides
     def _read(self, subset):
         np.random.seed(self.random_seed)
-        data_indices = self.get_data_indices(subset)
+        data_indices, data, labels = self.get_data_indices(subset)
         for idx in data_indices:
-            txt = newsgroups_data.data[idx]
-            topic = newsgroups_data.target[idx]
-            label = newsgroups_data['target_names'][topic].split(".")[0]
-            txt = clean_text(txt, special_chars=["\n", "\t"])
-            if len(txt) == 0 or len(label) == 0:
+            sentence = data[idx]
+            label = labels[idx]
+            if len(sentence) == 0 or len(label) == 0:
                 continue
-            yield self.text_to_instance(txt, label)
+            yield self.text_to_instance(sentence, label)
 
     def text_to_instance(
             self, string: str, label:str = None) -> Optional[Instance]:
@@ -126,4 +121,3 @@ class NewsgroupsDatasetReader(DatasetReader):
         if label is not None:
             fields["label"] = LabelField(label)
         return Instance(fields)
-
