2abc0fcaa98672eeffae9cd4ac4fdb8255345db7
crisz <crissstian96@gmail.com>
2021-11-25 18:15:21 +0100

    fix

--- src/predictors/misog/misog_dataset_reader.py
+++ src/predictors/misog/misog_dataset_reader.py
@@ -8,7 +8,7 @@ from nltk.tree import Tree
 
 from allennlp.common.file_utils import cached_path
 from allennlp.data.dataset_readers.dataset_reader import DatasetReader
-from allennlp.data.fields import LabelField, TextField, Field
+from allennlp.data.fields import LabelField, TextField, Field, IndexField
 from allennlp.data.instance import Instance
 from allennlp.data.token_indexers import TokenIndexer, SingleIdTokenIndexer
 from allennlp.data.tokenizers import Token
@@ -101,7 +101,7 @@ class MisogDatasetReader(DatasetReader):
         labels = [None] * len(data_indices)
         for i, idx in enumerate(data_indices):
             sentence = data[idx]
-            label = labels[idx][0]
+            label = int(labels[idx][0])
             strings[i] = sentence
             labels[i] = label
 
@@ -119,16 +119,16 @@ class MisogDatasetReader(DatasetReader):
         data_indices, data, labels = self.get_data_indices(subset)
         for idx in data_indices:
             sentence = data[idx]
-            label = labels[idx][0]
+            label = int(labels[idx][0])
             if len(sentence) == 0:
                 continue
             yield self.text_to_instance(sentence, label)
 
     def text_to_instance(
-            self, string: str, label:str = None) -> Optional[Instance]:
+            self, string: str, label = None) -> Optional[Instance]:
         tokens = self._tokenizer.tokenize(string)
         text_field = TextField(tokens, token_indexers=self._token_indexers)
         fields: Dict[str, Field] = {"tokens": text_field}
         if label is not None:
-            fields["label"] = LabelField(label)
+            fields["label"] = LabelField(str(label))
         return Instance(fields)
