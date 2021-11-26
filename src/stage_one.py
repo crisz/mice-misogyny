c51421cc503afb78bb15d0778a86a2b0a1653ce0
crisz <crissstian96@gmail.com>
2021-11-26 16:24:23 +0100

    new logs

--- src/stage_one.py
+++ src/stage_one.py
@@ -34,6 +34,7 @@ def train_epoch(epoch, editor_tokenizer, editor_model, device, loader, optim):
         lm_labels = data['target_ids'].to(device, dtype = torch.long)
         lm_labels[lm_labels[:, :] == editor_tokenizer.pad_token_id] = -100
         ids = data['source_ids'].to(device, dtype = torch.long)
+        print("Shape: ", ids.shape, lm_labels.shape)
         outputs = editor_model(input_ids = ids, labels=lm_labels)
         loss = outputs[0]
         total_loss += loss.item()
@@ -59,6 +60,7 @@ def validate_epoch(epoch, editor_tokenizer, editor_model, device, loader):
         lm_labels = data['target_ids'].to(device, dtype = torch.long)        
         lm_labels[lm_labels[:, :] == editor_tokenizer.pad_token_id] = -100
         ids = data['source_ids'].to(device, dtype = torch.long)
+        print("Shape: ", ids.shape, lm_labels.shape)
 
         outputs = editor_model(input_ids = ids, labels=lm_labels)
         loss = outputs[0]
